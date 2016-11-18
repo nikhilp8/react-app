36eff72943066dbefdde1ce38b18a7951e28affa
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-11-18 17:05:21 -0600

    Used index route and hash history

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,7 @@ var Main = React.createClass({
   render: function () {
     return (
       <div className='main-container'>
-        Hi there!!!{this.props.children}
+        Main component{this.props.children}
       </div>
     )
   }
