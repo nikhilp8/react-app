094cfc89e8405a17a948fab10e3d734a1bffb01d
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-11-19 22:48:20 -0600

    Get initialState & update any input changes wrt setState in Promptcontainer

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,7 +2,22 @@ var React = require('react');
 var transparentBg = require('../styles/index').transparentBg;
 
 var PromptContainer = React.createClass({
+
+
+	getInitialState: function(){
+		return {
+			username: ''
+		}
+	},
+
+	onUpdateUser: function(e) {
+		this.setState({
+			username: e.target.value
+		});
+	},
+
 	render:function(){
+
 		return(<div>
 				<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
 					<h1>{this.props.route.header}</h1>
@@ -12,6 +27,9 @@ var PromptContainer = React.createClass({
 							<input className="form-control"
 							   placeholder="Github username"
 							   type="text"
+							   onChange={this.onUpdateUser}
+							   value={this.state.username}
+
 							   />
 						</div>
 						<div className="form-group col-md-2 col-md-offset-5">
